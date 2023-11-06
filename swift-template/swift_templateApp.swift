4987fdd69ced8820f683e307129f590a19f5358e
Daniel Hurtado <a01707774@tec.mx>
2023-11-06 09:24:49 -0600

    Initial Commit

---
+++ swift-template/swift_templateApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_templateApp.swift
+//  swift-template
+//
+//  Created by Daniel Hurtado on 06/11/23.
+//
+
+import SwiftUI
+
+@main
+struct swift_templateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
